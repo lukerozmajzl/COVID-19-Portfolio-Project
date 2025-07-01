96705cc34b61188035a0d1a19cc8710afe79df6f
lukerozmajzl <ltrozmajzl@gmail.com>
2025-06-30 20:41:47 -0400

    Update COVID-19 Portfolio Project.sql
    I fixed up some SQL queries where 'WHERE continent IS NOT NULL' was left out. I also added a query that looks at death percentages by latitude using a new dataset from Kaggle.

--- COVID-19 Portfolio Project.sql
+++ COVID-19 Portfolio Project.sql
@@ -3,18 +3,6 @@ FROM PortfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
 ORDER BY location, date;
 
---Select data that we will be using
-SELECT
-  location,
-  date,
-  total_cases,
-  new_cases,
-  total_deaths,
-  population
-FROM PortfolioProject..CovidDeaths
-WHERE continent IS NOT NULL
-ORDER BY location, date;
-
  --Total Cases vs. Total Deaths
 SELECT
   location,
@@ -23,7 +11,7 @@ SELECT
   total_deaths,
   ROUND((total_deaths / total_cases) * 100.0, 2) AS death_percentage
 FROM PortfolioProject..CovidDeaths
-WHERE location LIKE '%states%'
+WHERE continent IS NOT NULL
 ORDER BY location, date;
 
  --Total Cases vs. Population
@@ -34,34 +22,40 @@ SELECT
   population,
   ROUND((total_cases / population) * 100.0, 2) AS percent_infected
 FROM PortfolioProject..CovidDeaths
+WHERE continent IS NOT NULL
 ORDER BY location, date;
 
  --Countries with Highest Infection Rates
 SELECT
+  continent,
   location,
   population,
   MAX(total_cases) AS highest_infection_count,
   ROUND(MAX((total_cases / population) * 100.0), 2) AS percent_infected
 FROM PortfolioProject..CovidDeaths
-GROUP BY location, population
+WHERE continent IS NOT NULL
+GROUP BY continent, location, population
 ORDER BY percent_infected DESC;
 
  --Countries with Highest Death Counts
 SELECT
+  continent,
   location,
   MAX(CAST(total_deaths AS int)) AS total_death_count
 FROM PortfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
-GROUP BY location
+GROUP BY continent, location
 ORDER BY total_death_count DESC;
 
  --Continents with Highest Death Counts
 SELECT
+  population,
   location,
-  MAX(CAST(total_deaths AS int)) AS total_death_count
+  MAX(CAST(total_deaths AS int)) AS total_death_count,
+  ROUND(MAX(CAST(total_deaths AS int)) / population * 100, 2) AS death_percentage
 FROM PortfolioProject..CovidDeaths
 WHERE continent IS NULL
-GROUP BY location
+GROUP BY population, location
 ORDER BY total_death_count DESC;
 
 --Global Numbers
@@ -86,14 +80,14 @@ WITH PercentVaccinated (
 ) AS ( 
   SELECT
     cd.continent,
-    cd.location,
-    cd.date,
-    cd.population,
-    cv.new_vaccinations,
-    SUM(CAST(new_vaccinations AS INT)) OVER (
-      PARTITION BY cd.location
-      ORDER BY cd.date
-    ) AS cumulative_vaccinations
+	  cd.location,
+	  cd.date,
+	  cd.population,
+	  cv.new_vaccinations,
+	  SUM(CAST(new_vaccinations AS INT)) OVER (
+	    PARTITION BY cd.location
+	    ORDER BY cd.date
+	  ) AS cumulative_vaccinations
   FROM PortfolioProject..CovidDeaths cd
   JOIN PortfolioProject..CovidVaccinations cv
     ON cd.location = cv.location
@@ -109,16 +103,30 @@ ORDER BY location, date;
 CREATE VIEW PercentVaccinated AS
 SELECT
   cd.continent,
-  cd.location,
-  cd.date,
-  cd.population,
-  cv.new_vaccinations,
-  SUM(CAST(new_vaccinations AS INT)) OVER (
-    PARTITION BY cd.location
-    ORDER BY cd.date
-  ) AS cumulative_vaccinations
+	cd.location,
+	cd.date,
+	cd.population,
+	cv.new_vaccinations,
+	SUM(CAST(new_vaccinations AS INT)) OVER (
+	  PARTITION BY cd.location
+	  ORDER BY cd.date
+	) AS cumulative_vaccinations
 FROM PortfolioProject..CovidDeaths cd
 JOIN PortfolioProject..CovidVaccinations cv
   ON cd.location = cv.location
-  AND cd.date = cv.date
+	AND cd.date = cv.date
 WHERE cd.continent IS NOT NULL
+
+--Death Percentage by Latitude
+SELECT
+  City_Name,
+  Country_Na,
+  Latitude,
+  Reported_D,
+  Population,
+  Total_Deat,
+  Total_Case,
+  ROUND((Total_Deat / Total_Case) * 100, 2) AS Death_Perc
+FROM PortfolioProject..CovidCityData
+WHERE Population != 0 AND Total_Case != 0
+ORDER BY Death_Perc DESC
